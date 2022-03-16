9cb0c8d7edd8541934be3701b134aa8471c921cf
amo <amo.ramishvili@hotmail.com>
2022-03-16 18:57:45 +0400

    fix bug

--- src/pagination.vue
+++ src/pagination.vue
@@ -2,7 +2,7 @@
 import { defineComponent } from "vue";
 
 export default /*#__PURE__*/ defineComponent({
-  name: "VuePagination",
+  name: "VPagination",
   props: {
     value: {
       type: Number,
