32031acd72f5981e575d3e3fdba13bddfabc88d2
amo <amo.ramishvili@hotmail.com>
2022-03-16 18:57:27 +0400

    add pagination component

---
+++ dev/serve.vue
@@ -0,0 +1,36 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import VPagination from "@/pagination.vue";
+
+export default defineComponent({
+  name: "ServeDev",
+  components: {
+    VPagination,
+  },
+  setup() {
+    const PaginationHandler = (pageNum: Number) => {
+      console.log(pageNum);
+    };
+
+    return {
+      PaginationHandler,
+    };
+  },
+});
+</script>
+
+<template>
+  <div id="app">
+    <VPagination
+      class="pagination"
+      :pageRange="10"
+      prevClass="prev"
+      nextClass="next"
+      nextText=""
+      prevText=""
+      :pageCount="{ autoCount: false, limit: 10, count: 20 }"
+      :selectedPage="1"
+      :clickHandler="PaginationHandler"
+    />
+  </div>
+</template>
